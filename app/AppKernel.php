837c0e1940e7a9f396d413ea914943beeb94124f
Sirawit7205 <lsirawit24@gmail.com>
2020-04-07 00:52:09 +0700

    remove dev from appkernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,13 +35,6 @@ class AppKernel extends Kernel
             new Bazinga\Bundle\JsTranslationBundle\BazingaJsTranslationBundle(),
         ];
 
-        // Developer bundles
-        if (in_array($this->getEnvironment(), ['dev', 'test'])) {
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Liip\FunctionalTestBundle\LiipFunctionalTestBundle();
-        }
-
         // PartKeepr bundles
         $bundles[] = new \PartKeepr\SetupBundle\PartKeeprSetupBundle();
         $bundles[] = new PartKeepr\CronLoggerBundle\PartKeeprCronLoggerBundle();
